44e849a434f2896b1bb1018b9cf1110388a2186b
Faustin <faustinkov17@gmail.com>
2023-07-27 11:52:18 +0100

    solve exercice

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//top
+const firstArray = ['La vie', 'Croire en sa destinee', 'Rester positif'];
+const secondArray = ['Croire en Dieu', 'Dire merci', 'Dire bonjour'];
+const thirdArray = ['Travailler', 'manger', 'Jeuner et beaucoup manger apres'];
+
+const getRandomIndex = (array) => {
+    return Math.floor(Math.random() * array.length);
+  };
+
+const message = `${firstArray[getRandomIndex(firstArray)]}, ${secondArray[getRandomIndex(secondArray)]}, ${thirdArray[getRandomIndex(thirdArray)]}.`;
+
+console.log(message);
